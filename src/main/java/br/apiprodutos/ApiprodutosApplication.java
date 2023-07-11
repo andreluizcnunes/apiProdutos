205716df03a02725ecab5e9700c4bf05ebb07219
Andre Nunes <andre.doctors@gmail.com>
2023-07-11 15:59:52 -0300

    project created

---
+++ src/main/java/br/apiprodutos/ApiprodutosApplication.java
@@ -0,0 +1,13 @@
+package br.apiprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/apiprodutos/ApiprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.apiprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
