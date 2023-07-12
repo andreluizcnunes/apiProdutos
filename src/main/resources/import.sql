11ff437b3b545b672c36703989cef322c8c5b8b3
André Luiz <andre.doctors@gmail.com>
2023-07-12 15:37:49 -0300

    atualizacao seed da base de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,26 @@
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+INSERT INTO tb_categoria (nome_categoria) VALUES ('Camisetas');
+INSERT INTO tb_categoria (nome_categoria) VALUES ('Calças');
+
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta estampada', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta de manga curta', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta de manga comprida', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta polo', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Camiseta regata', 29.99, 'https://exemplo.com/camiseta.jpg', 'https://exemplo.com/video_camiseta.mp4', 'unidade', 'disponível');
+
 INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Calça Jeans', 59.99, 'https://exemplo.com/calca_jeans.jpg', 'https://exemplo.com/video_calca_jeans.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Tênis Esportivo', 99.99, 'https://exemplo.com/tenis_esportivo.jpg', 'https://exemplo.com/video_tenis_esportivo.mp4', 'par', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Mochila', 49.99, 'https://exemplo.com/mochila.jpg', 'https://exemplo.com/video_mochila.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Relógio de Pulso', 79.99, 'https://exemplo.com/relogio_pulso.jpg', 'https://exemplo.com/video_relogio_pulso.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Óculos de Sol', 39.99, 'https://exemplo.com/oculos_sol.jpg', 'https://exemplo.com/video_oculos_sol.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Bolsa Feminina', 69.99, 'https://exemplo.com/bolsa_feminina.jpg', 'https://exemplo.com/video_bolsa_feminina.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Boné Esportivo', 19.99, 'https://exemplo.com/bone_esportivo.jpg', 'https://exemplo.com/video_bone_esportivo.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Blusa de Frio', 39.99, 'https://exemplo.com/blusa_frio.jpg', 'https://exemplo.com/video_blusa_frio.mp4', 'unidade', 'disponível');
-INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Chinelo', 9.99, 'https://exemplo.com/chinelo.jpg', 'https://exemplo.com/video_chinelo.mp4', 'par', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Calça social', 59.99, 'https://exemplo.com/calca_jeans.jpg', 'https://exemplo.com/video_calca_jeans.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Calça leggings', 59.99, 'https://exemplo.com/calca_jeans.jpg', 'https://exemplo.com/video_calca_jeans.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Calça cargo', 59.99, 'https://exemplo.com/calca_jeans.jpg', 'https://exemplo.com/video_calca_jeans.mp4', 'unidade', 'disponível');
+INSERT INTO tb_produto (nome_produto, valor_produto, image_url_produto, video_url_produto, unidade_medida_produto, status_produto) VALUES ('Calça culotte', 59.99, 'https://exemplo.com/calca_jeans.jpg', 'https://exemplo.com/video_calca_jeans.mp4', 'unidade', 'disponível');
+
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (1, 1, 0);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (1, 2, 1);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (1, 3, 2);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (1, 4, 3);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (1, 5, 4);
+
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (2, 6, 0);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (2, 7, 1);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (2, 8, 2);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (2, 9, 3);
+INSERT INTO tb_belonging (categoria_id, produto_id, position) VALUES (2, 10, 4);
